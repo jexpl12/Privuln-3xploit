47b9021c7868342ca82f63117a86e6eeea1ca29f
Baratha Wijaya <107695774+jexpl12@users.noreply.github.com>
2023-08-01 21:14:07 +0700

    Update privuln.sh

--- privuln.sh
+++ privuln.sh
@@ -4,7 +4,7 @@ RED="$(printf '\033[1;31m')"  GREEN="$(printf '\033[1;32m')" PURPLE="$(printf '\
 cat << EOF
 ${PURPLE}    ___   ___   _ ${RED}  _    ${GREEN}  _     _     _
 ${PURPLE}   | |_) | |_) / |${RED} \ \  /${GREEN} | | | | |   | |\ |
-${PURPLE}   |_|   |_| \ |_|${RED}  \_\/ ${GREEN} \_\_/ |_|__ |_| \|  ${RESAT}1337r0j4n
+${PURPLE}   |_|   |_| \ |_|${RED}  \_\/ ${GREEN} \_\_/ |_|__ |_| \|  ${RESAT} Jexpl12
 EOF
 read -p " Target: " target
 check=$(curl -s -o /dev/null --max-time 10 -w "%{http_code}" $target/$vulnpath)
